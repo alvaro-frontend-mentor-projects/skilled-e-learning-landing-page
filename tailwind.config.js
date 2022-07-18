538cbbc3b80363039379dc4ec33549a2448bd559
alvaroormeno <alvaro@ormeno.org>
2022-07-18 13:24:53 -0400

    added custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+      screens: {
+        'sm': '375px',
+        // => @media (min-width: 576px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 960px) { ... }
+  
+        'lg': '1440px',
+        // => @media (min-width: 1440px) { ... }
+      },
+    extend: {
+      colors: {
+        fmLightBlue: "#666ca3",
+        fmDarkBlue: "#13183F",
+        fmGray: "#83869A",
+        fmPink: "#F74780",
+        fmLightPink: "#FFA7C3",
+      }
+    },
   },
   plugins: [],
 }
